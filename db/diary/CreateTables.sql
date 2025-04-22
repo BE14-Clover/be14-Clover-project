d06742e0030a175bca7ca53d2efd989b302eb4ff
Gombo2 <rhtjddus0502@gmail.com>
2025-04-23 05:51:52 +0900

    feat: 일기관련 테이블 생성

--- db/diary/CreateTables.sql
+++ db/diary/CreateTables.sql
@@ -4,6 +4,22 @@ DROP TABLE IF EXISTS picture CASCADE;
 DROP TABLE IF EXISTS diary_tag CASCADE;
 DROP TABLE IF EXISTS tag CASCADE;
 DROP TABLE IF EXISTS sticker CASCADE;
+DROP TABLE IF EXISTS diary CASCADE;
+
+-- diary 테이블 생성
+CREATE TABLE IF NOT EXISTS diary
+(
+    id              INTEGER      NOT NULL AUTO_INCREMENT,
+    title           VARCHAR(255) NULL COMMENT '제목 없으면 해당 날짜 DATE 형식으로 작성됨',
+    content         TEXT         NULL,
+    created_at      DATETIME     NULL,
+    is_deleted      BOOLEAN      NULL COMMENT '소프트 딜리트',
+    is_confirmed    BOOLEAN      NULL COMMENT '확정 O, 확정 X (04시에 스케줄러로 일괄처리)',
+    sticker_layer   TEXT         NULL,
+    user_id         INTEGER      NOT NULL,
+    CONSTRAINT pk_diary_id PRIMARY KEY (id),
+    CONSTRAINT fk_diary_user_id FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE
+);
 
 -- emotion_analyze 테이블 생성
 CREATE TABLE IF NOT EXISTS emotion_analyze
@@ -26,17 +42,6 @@ CREATE TABLE IF NOT EXISTS picture
     CONSTRAINT fk_picture_diary_id FOREIGN KEY (diary_id) REFERENCES diary (id) ON DELETE CASCADE
 );
 
--- diary_tag 테이블 생성
-CREATE TABLE IF NOT EXISTS diary_tag
-(
-    id         INTEGER  NOT NULL AUTO_INCREMENT,
-    diary_id   INTEGER  NOT NULL,
-    tag_id     VARCHAR(255)  NOT NULL,
-    CONSTRAINT pk_diary_tag_id PRIMARY KEY (id),
-    CONSTRAINT fk_diary_tag_diary_id FOREIGN KEY (diary_id) REFERENCES diary (id) ON DELETE CASCADE,
-    CONSTRAINT fk_diary_tag_tag_id FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE
-);
-
 -- tag 테이블 생성
 CREATE TABLE IF NOT EXISTS tag
 (
@@ -45,11 +50,22 @@ CREATE TABLE IF NOT EXISTS tag
     CONSTRAINT pk_tag_id PRIMARY KEY (id)
 );
 
+-- diary_tag 테이블 생성
+CREATE TABLE IF NOT EXISTS diary_tag
+(
+    id         INTEGER      NOT NULL AUTO_INCREMENT,
+    diary_id   INTEGER      NOT NULL,
+    tag_id     VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_diary_tag_id PRIMARY KEY (id),
+    CONSTRAINT fk_diary_tag_diary_id FOREIGN KEY (diary_id) REFERENCES diary (id) ON DELETE CASCADE,
+    CONSTRAINT fk_diary_tag_tag_id FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE
+);
+
 -- sticker 테이블 생성
 CREATE TABLE IF NOT EXISTS sticker
 (
-    id          INTEGER  NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255)  NULL,
-    svg_code    TEXT     NULL,
+    id          INTEGER      NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NULL,
+    svg_code    TEXT         NULL,
     CONSTRAINT pk_sticker_id PRIMARY KEY (id)
 );
