b4952b8965fdb8518ec2ea1388ef7675849500d1
JakeGo <161000881+jakego1@users.noreply.github.com>
2025-04-23 08:59:16 +0900

    회원 관련 더미, 제약조건 초안 추가
    user, pet, register_questions, DDL 추가

---
+++ db/diary/DUMMY/pet.sql
@@ -0,0 +1,12 @@
+INSERT 
+INTO pet 
+(
+  id
+, pet_name
+, pet_img
+)
+VALUES
+(1, '공룡', 'https://i.pinimg.com/originals/05/79/d4/0579d4e6568c980df96b447fbc358014.gif'),
+(2, '강아지', 'https://i.pinimg.com/736x/23/db/6e/23db6e1fe41c5ce16f1f6b577fd95e0f.jpg'),
+(3, '오리', 'https://i.pinimg.com/originals/71/88/0f/71880f60fc9fa0973ef856dffed68e80.gif'),
+(4, '고양이', 'https://i.pinimg.com/736x/30/14/89/301489e41d6da1b4177ca7655daad80d.jpg');
