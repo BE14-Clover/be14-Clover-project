b4c6e15486841713b5d8b0fa34ebae15c7c34196
SungMin-hub <sungmin806@gmail.com>
2025-04-23 17:33:45 +0900

    spring boot 기본 configure commit

---
+++ moodiary/src/main/java/com/clover/moodiary/MoodiaryApplication.java
@@ -0,0 +1,13 @@
+package com.clover.moodiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodiaryApplication.class, args);
+	}
+
+}
