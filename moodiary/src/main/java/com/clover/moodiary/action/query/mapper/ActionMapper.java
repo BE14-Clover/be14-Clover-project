1b14afc0510f7f61e3dfba33832b3662c81fb5f2
Bluesky3125 <charlie4822@gmail.com>
2025-04-28 04:27:51 +0900

    Feature: 추천 행동 관련 알고리즘 진행 중 1
    Feature: 추천 행동 가중치 플러스 기능
    Feature: 추천 행동 랜덤 선택 기능
    Refactor: 추천 행동 가중치 초기화 기능 테스트 완료
    Refactor: Application 시큐리티 설정 전까지 로그인 페이지 안뜨게 임시 조치

--- moodiary/src/main/java/com/clover/moodiary/action/query/mapper/ActionMapper.java
+++ moodiary/src/main/java/com/clover/moodiary/action/query/mapper/ActionMapper.java
@@ -4,7 +4,24 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.clover.moodiary.action.command.application.dto.ActionTagDTO;
+import com.clover.moodiary.action.command.application.dto.RecommendedActionDTO;
+import com.clover.moodiary.action.command.application.dto.TaggedRecommendedActionsDTO;
+import com.clover.moodiary.action.command.application.dto.UserPreferencesDTO;
+
 @Mapper
 public interface ActionMapper {
-	List<Integer> selectActionTagIdList();
+	List<ActionTagDTO> selectActionTagIdList();
+	
+	RecommendedActionDTO selectRecommendedActionById(int actionId);
+	
+	List<TaggedRecommendedActionsDTO> selectActionTagByActionId(int actionId);
+	
+	UserPreferencesDTO selectUserPreferencesByActionTagId(int param1, int parma2);
+	
+	ActionTagDTO selectParentActionTagByActionTag(int actionTagId);
+	
+	List<ActionTagDTO> selectActionTagByParentActionId(int parentActionTagId);
+	
+	List<TaggedRecommendedActionsDTO> selectRecommendedActionIdByActionTagId(int randomActionTagId);
 }
