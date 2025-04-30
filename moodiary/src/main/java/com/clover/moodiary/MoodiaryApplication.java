0e18d62bb57b6edf98488085f0ce33fa15283e85
50071 <rkd50071@gmail.com>
2025-04-30 11:11:02 +0900

    스티커 관련 프론트 연동

--- moodiary/src/main/java/com/clover/moodiary/MoodiaryApplication.java
+++ moodiary/src/main/java/com/clover/moodiary/MoodiaryApplication.java
@@ -3,9 +3,11 @@ package com.clover.moodiary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /* 설명. 시큐리티 설정 전까지 로그인 페이지 안 뜨게*/
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableConfigurationProperties
 public class MoodiaryApplication {
 
 	public static void main(String[] args) {
