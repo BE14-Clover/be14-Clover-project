6cf4774fc59d74db1fc774a5c9c3ccd04edf6a91
SungMin-hub <sungmin806@gmail.com>
2025-04-30 20:31:38 +0900

    security 적용중 commit

--- moodiary/src/main/java/com/clover/moodiary/shareddiaryroom/query/controller/SharedDiaryRoomQueryController.java
+++ moodiary/src/main/java/com/clover/moodiary/shareddiaryroom/query/controller/SharedDiaryRoomQueryController.java
@@ -3,6 +3,8 @@ package com.clover.moodiary.shareddiaryroom.query.controller;
 import com.clover.moodiary.shareddiaryroom.query.dto.SharedDiaryRoomResponse;
 import com.clover.moodiary.shareddiaryroom.query.service.SharedDiaryRoomQueryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,11 +14,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SharedDiaryRoomQueryController {
 
-    private final SharedDiaryRoomQueryService sharedDiaryRoomService;
+    private final SharedDiaryRoomQueryService sharedDiaryRoomQueryService;
 
     @GetMapping
-    public List<SharedDiaryRoomResponse> findRoomByUserId(@RequestParam(value="userId") Integer userId) {
-        return sharedDiaryRoomService.findRoomByUserId(userId);
+    public List<SharedDiaryRoomResponse> findRoomByUserId() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Integer userId = (Integer)auth.getPrincipal();
+
+        return sharedDiaryRoomQueryService.findRoomByUserId(userId);
     }
 
 
