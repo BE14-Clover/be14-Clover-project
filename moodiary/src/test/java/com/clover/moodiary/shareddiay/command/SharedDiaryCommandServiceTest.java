43533d6b9f1ebd4744dac690739afaf45959f94a
SungMin-hub <sungmin806@gmail.com>
2025-05-02 01:51:22 +0900

    공유 일기 테스트 케이스 commit

--- moodiary/src/test/java/com/clover/moodiary/shareddiay/command/SharedDiaryCommandServiceTest.java
+++ moodiary/src/test/java/com/clover/moodiary/shareddiay/command/SharedDiaryCommandServiceTest.java
@@ -13,6 +13,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @SpringBootTest
 public class SharedDiaryCommandServiceTest {
@@ -23,70 +27,74 @@ public class SharedDiaryCommandServiceTest {
     @Autowired
     private SharedDiaryRepository sharedDiaryRepository;
 
-//    @ParameterizedTest
-//    @DisplayName("공유 일기 작성 테스트")
-//    @ValueSource(ints = {1})
-//    public void createSharedDiaryTest(int userId) {
-//        Integer roomId = 1;
-//        String title = "오늘의 일기";
-//        String content = "따뜻한 커피와 함께 시작한 하루.";
-//        String styleLayer = "[{\"type\":\"sticker\",\"src\":\"heart.png\",\"x\":100,\"y\":120}]";
-//
-//        CreateSharedDiaryRequest request = new CreateSharedDiaryRequest(
-//                roomId,
-//                userId,
-//                title,
-//                content,
-//                styleLayer
-//        );
-//
-//        Integer diaryId = sharedDiaryCommandService.createDiary(request).getId();
-//
-//        SharedDiary diary = sharedDiaryRepository.findById(diaryId).orElse(null);
-//        Assertions.assertNotNull(diary);
-//        Assertions.assertEquals(userId, diary.getUserId());
-//        Assertions.assertEquals(title, diary.getTitle());
-//        Assertions.assertEquals(content, diary.getContent());
-//        Assertions.assertEquals(styleLayer, diary.getStyleLayer());
-//        Assertions.assertEquals(roomId, diary.getSharedDiaryRoomId());
-//    }
-//
-//    @ParameterizedTest
-//    @DisplayName("공유 일기 수정 테스트")
-//    @ValueSource(ints = {10})
-//    public void updateSharedDiaryTest(int diaryId) {
-//        int userId = 1;
-//
-//        UpdateSharedDiaryRequest request = new UpdateSharedDiaryRequest(
-//                diaryId,
-//                userId,
-//                "수정된 일기 제목",
-//                "오늘은 정말 잊지 못할 하루였다. 커피 향이 마음을 가득 채웠지.",
-//                "[{\"type\":\"sticker\",\"src\":\"star.png\",\"x\":150,\"y\":200}]"
-//        );
-//
-//        UpdateSharedDiaryReponse response = sharedDiaryCommandService.updateDiary(request);
-//
-//        SharedDiary diary = sharedDiaryRepository.findById(response.getDiaryId()).orElse(null);
-//        Assertions.assertNotNull(diary);
-//        Assertions.assertEquals(request.getTitle(), diary.getTitle());
-//        Assertions.assertEquals(request.getContent(), diary.getContent());
-//        Assertions.assertEquals(request.getStyleLayer(), diary.getStyleLayer());
-//        Assertions.assertEquals(diaryId, response.getDiaryId());
-//    }
-//
-//    @ParameterizedTest
-//    @DisplayName("공유 일기 삭제 테스트")
-//    @ValueSource(ints = {10})
-//    public void deleteSharedDiaryTest(int diaryId) {
-//        int userId = 1;
-//
-//        DeleteSharedDiaryRequest request = new DeleteSharedDiaryRequest(diaryId, userId);
-//
-//        sharedDiaryCommandService.deleteDiary(request);
-//
-//        SharedDiary deletedDiary = sharedDiaryRepository.findById(diaryId).orElse(null);
-//        Assertions.assertNotNull(deletedDiary);
-//        Assertions.assertEquals("Y", deletedDiary.getIsDeleted());
-//    }
+    @ParameterizedTest
+    @DisplayName("공유 일기 작성 테스트")
+    @ValueSource(ints = {1})
+    public void createSharedDiaryTest(int userId) throws IOException {
+        Integer roomId = 1;
+        String title = "오늘의 일기";
+        String content = "따뜻한 커피와 함께 시작한 하루.";
+        String styleLayer = "[{\"type\":\"sticker\",\"src\":\"heart.png\",\"x\":100,\"y\":120}]";
+
+        CreateSharedDiaryRequest request = new CreateSharedDiaryRequest(
+                roomId,
+                userId,
+                title,
+                content,
+                styleLayer
+        );
+
+        MultipartFile image = null;
+
+        Integer diaryId = sharedDiaryCommandService.createDiary(request, image).getId();
+
+        SharedDiary diary = sharedDiaryRepository.findById(diaryId).orElse(null);
+        Assertions.assertNotNull(diary);
+        Assertions.assertEquals(userId, diary.getUserId());
+        Assertions.assertEquals(title, diary.getTitle());
+        Assertions.assertEquals(content, diary.getContent());
+        Assertions.assertEquals(styleLayer, diary.getStyleLayer());
+        Assertions.assertEquals(roomId, diary.getSharedDiaryRoomId());
+    }
+
+    @ParameterizedTest
+    @DisplayName("공유 일기 수정 테스트")
+    @ValueSource(ints = {10})
+    public void updateSharedDiaryTest(int diaryId) throws IOException {
+        int userId = 1;
+
+        UpdateSharedDiaryRequest request = new UpdateSharedDiaryRequest(
+                diaryId,
+                userId,
+                "수정된 일기 제목",
+                "오늘은 정말 잊지 못할 하루였다. 커피 향이 마음을 가득 채웠지.",
+                "[{\"type\":\"sticker\",\"src\":\"star.png\",\"x\":150,\"y\":200}]"
+        );
+
+        MultipartFile image = null;
+
+        UpdateSharedDiaryReponse response = sharedDiaryCommandService.updateDiary(request, image);
+
+        SharedDiary diary = sharedDiaryRepository.findById(response.getDiaryId()).orElse(null);
+        Assertions.assertNotNull(diary);
+        Assertions.assertEquals(request.getTitle(), diary.getTitle());
+        Assertions.assertEquals(request.getContent(), diary.getContent());
+        Assertions.assertEquals(request.getStyleLayer(), diary.getStyleLayer());
+        Assertions.assertEquals(diaryId, response.getDiaryId());
+    }
+
+    @ParameterizedTest
+    @DisplayName("공유 일기 삭제 테스트")
+    @ValueSource(ints = {10})
+    public void deleteSharedDiaryTest(int diaryId) {
+        int userId = 1;
+
+        DeleteSharedDiaryRequest request = new DeleteSharedDiaryRequest(diaryId, userId);
+
+        sharedDiaryCommandService.deleteDiary(request);
+
+        SharedDiary deletedDiary = sharedDiaryRepository.findById(diaryId).orElse(null);
+        Assertions.assertNotNull(deletedDiary);
+        Assertions.assertEquals("Y", deletedDiary.getIsDeleted());
+    }
 }
