8b0a08cd027257f73b0ba568df9256c1242b3e68
Gombo2 <rhtjddus0502@gmail.com>
2025-05-02 02:02:37 +0900

    feat : 테스트케이스 작성 및 DTO Builder 추가

--- moodiary/src/main/java/com/clover/moodiary/user/command/dto/LoginRequest.java
+++ moodiary/src/main/java/com/clover/moodiary/user/command/dto/LoginRequest.java
@@ -1,10 +1,16 @@
 package com.clover.moodiary.user.command.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class LoginRequest {
 	private String email;
 	private String password;
--- moodiary/src/main/java/com/clover/moodiary/user/query/dto/RequestPasswordDTO.java
+++ moodiary/src/main/java/com/clover/moodiary/user/query/dto/RequestPasswordDTO.java
@@ -1,10 +1,16 @@
 package com.clover.moodiary.user.query.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RequestPasswordDTO {
 	private String name;
 	private String phoneNumber;
