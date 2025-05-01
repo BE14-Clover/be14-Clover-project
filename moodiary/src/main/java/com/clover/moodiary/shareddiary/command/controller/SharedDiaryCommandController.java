347cee1d7a00677c7f0efe5b9e085508f40c7719
SungMin-hub <sungmin806@gmail.com>
2025-05-01 13:41:07 +0900

    공유 일기 로그인 적용 commit

--- moodiary/src/main/java/com/clover/moodiary/shareddiary/command/controller/SharedDiaryCommandController.java
+++ moodiary/src/main/java/com/clover/moodiary/shareddiary/command/controller/SharedDiaryCommandController.java
@@ -4,6 +4,8 @@ import com.clover.moodiary.shareddiary.command.dto.*;
 import com.clover.moodiary.shareddiary.command.service.SharedDiaryCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,19 +17,45 @@ public class SharedDiaryCommandController {
 
     @PostMapping("/create")
     public ResponseEntity<CreateSharedDiaryResponse> createDiary(@RequestBody CreateSharedDiaryRequest request) {
-        CreateSharedDiaryResponse response = sharedDiaryCommandService.createDiary(request);
-        return ResponseEntity.ok(response);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Integer userId = (Integer) auth.getPrincipal();
+
+        CreateSharedDiaryRequest fixedRequest = new CreateSharedDiaryRequest(
+                request.getRoomId(),
+                userId,
+                request.getTitle(),
+                request.getContent(),
+                request.getStyleLayer()
+        );
+        return ResponseEntity.ok(sharedDiaryCommandService.createDiary(fixedRequest));
     }
 
     @PutMapping("/update")
     public ResponseEntity<UpdateSharedDiaryReponse> updateDiary(@RequestBody UpdateSharedDiaryRequest request) {
-        UpdateSharedDiaryReponse response = sharedDiaryCommandService.updateDiary(request);
-        return ResponseEntity.ok(response);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Integer userId = (Integer) auth.getPrincipal();
+
+        request = new UpdateSharedDiaryRequest(
+                request.getDiaryId(),
+                userId,
+                request.getTitle(),
+                request.getContent(),
+                request.getStyleLayer()
+        );
+        return ResponseEntity.ok(sharedDiaryCommandService.updateDiary(request));
     }
 
     @PutMapping("/delete")
     public ResponseEntity<DeleteSharedDiaryResponse> deleteDiary(@RequestBody DeleteSharedDiaryRequest request) {
-        return ResponseEntity.ok(sharedDiaryCommandService.deleteDiary(request));
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Integer userId = (Integer) auth.getPrincipal();
+
+        DeleteSharedDiaryRequest fixedRequest = new DeleteSharedDiaryRequest(
+                request.getDiaryId(),
+                userId
+        );
+
+        return ResponseEntity.ok(sharedDiaryCommandService.deleteDiary(fixedRequest));
     }
 
 }
